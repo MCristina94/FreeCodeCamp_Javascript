2c14c46c1c7e803b97cace2d35a06861d5cd0b19
Mcristina94 <cmaureen1908@gmail.com>
2023-01-26 15:34:40 -0500

    ejercicios realizados desde el 20 al 26, donde se trabaja sobre exportaciones e importaciones

---
+++ ES6/24.Export_Default.js
@@ -0,0 +1,6 @@
+/*La siguiente función debe ser el valor fallback para el módulo. 
+Por favor, añade el código necesario para hacerlo. */
+
+export default function subtract(x, y) {
+    return x - y;
+  }
---
+++ ES6/26.Promesas.js
@@ -0,0 +1,6 @@
+/*Crea una nueva promesa llamada makeServerRequest. 
+Pásale una función con parámetros resolve y reject al constructor. */
+
+const makeServerRequest = new Promise ((resolve,reject)=> {
+
+});
